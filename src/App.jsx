f949c792ca36af6090849964b3c61cf4221795dd
chhong <81909140+pnci1029@users.noreply.github.com>
2024-03-19 14:44:49 +0900

    24.03.19 simple setting for players

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Player from "./components/Player";
+
 function App() {
 
 
     return (
         <main>
             <div id="game-container">
-                PLAYERS
+                <ol id="players">
+                    <Player name="Player 1" symbol="X"/>
+                    <Player name="Player 2" symbol="O"/>
+                </ol>
 
                 GAME BOARD
             </div>
