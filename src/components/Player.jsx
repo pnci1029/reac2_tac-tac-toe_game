3a5c1e2245fc517a9c6695251486cb78bdbfdfe0
chhong <81909140+pnci1029@users.noreply.github.com>
2024-03-19 15:05:23 +0900

    24.03.19 simple setting for players

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,30 @@
+import {useState} from "react";
+
 export default function Player({name, symbol}) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [isName, setIsName] = useState(name);
+
+    let playerName = <span className="player-name">{name}</span>;
+    if (isEditing) {
+        playerName = <input type="text" required/>
+    }
+
+
+    function handleEditClick(){
+        setIsEditing(true)
+        // isEditing ? setIsEditing(false) : setIsEditing(true);
+        // setIsName()
+    }
     return (
         <li>
             <span>
-                <span className="player-name">{name}</span>
+                {/*{!isEditing ? <span className="player-name">{name}</span>*/}
+                {/*: <input/>}*/}
+                {playerName}
+
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ?  "Save"  : "Edit"}</button>
         </li>
     )
 };
