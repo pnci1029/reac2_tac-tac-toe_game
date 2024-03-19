d342ab2fb668189b9ee96f3f18fb14cbd46d73aa
chhong <81909140+pnci1029@users.noreply.github.com>
2024-03-19 14:32:45 +0900

    24.03.19 set up for dev

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+
+
+    return (
+        <main>
+            <div id="game-container">
+                PLAYERS
+
+                GAME BOARD
+            </div>
+            LOG
+        </main>
+    )
+}
+
+export default App
